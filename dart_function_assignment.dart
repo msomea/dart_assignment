1272469ccf3c5e073a44da41a611a558bfc3933a
msomea <msomearaphael@gmail.com>
2023-03-12 14:51:57 +0300

    Edit Function asigment 1 to 4

---
+++ dart_function_assignment.dart
@@ -0,0 +1,49 @@
+// Dart Function Assigment
+void main()
+{
+  //Add two function
+int addTwo (int a, int b)
+{
+  int sum;
+  sum = a + b;
+  return sum;
+}
+print("Addition answer: ${addTwo(2,3)}");
+//Subtract two function
+int subtractTwo (int a, int b)
+{
+  int different;
+  different = a - b;
+  return different;
+}
+print("Subtraction answer: ${subtractTwo(7, 3)}");
+//Multiply Function
+int multiplyTwo(int a, int b)
+{
+int product;
+product = a * b;
+return product;
+}
+print("Multiplication answer: ${multiplyTwo(7, 9)}");
+//Division Function
+double devideTwo(double a, double b)
+{
+  double answer;
+  answer = a / b;
+  return answer;
+}
+print("Division answer: ${devideTwo(9, 3)}");
+//String Length Function
+int stringLength(String a)
+{
+  String a;
+  a = "Raphael";
+  int  aLength;
+  aLength =a.length;
+  return aLength;
+}
+print("Length of string Raphael is ${stringLength("Raphael")}");
+//Get First Ellement Function
+
+
+}
